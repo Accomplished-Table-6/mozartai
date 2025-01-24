bd47507f590dfc41c1bfb265008ee41ad823d3a3
Accomplished-Table-6 <edwinsalisbury83@gmail.com>
2025-01-24 13:57:30 -0600

    Wrote starting point for xml encoder

--- xmlencoder.py
+++ xmlencoder.py
@@ -1,2 +1,12 @@
+import xml.etree.ElementTree as ET
 
+def create_musicxml(input_notation):
+    # Create the rooot element
+    score_partwise = ET.Element("score-partwise",version="3.1")
+
+    # Initialize the part
+    part = ET.SubElement(score_partwise,"part",id="P1")
+
+    # Initialize variables for the current measures and their content
+    
 
