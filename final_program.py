eec57f5153bb6e04791674612647d29576b4515a
Accomplished-Table-6 <72772964+Accomplished-Table-6@users.noreply.github.com>
2025-02-02 17:34:34 -0600

    Update final_program.py

--- final_program.py
+++ final_program.py
@@ -2,18 +2,18 @@ import mozart_predict as mp
 import xmlencoder as xl
 import tempfile
 import image_dissector as imd
+import os
 
 
-image_path = "Data/Example/Example2.png"
-Temp_path = tempfile.TemporaryDirectory(dir ="Data/temp")
+image_path = "Data/Example/Capture.PNG"
+Temp_path = "Data/Temp"
 model_path = "Models/semantic_model.meta"
 voc_file_path = "Data/vocabulary_semantic.txt"
 measure_list = []
-file_list = imd.split_sheet_music_from_folder(image_path,Temp_path)
-
+file_list = imd.split_sheet_music_per_measure(image_path, Temp_path, multi_measure_rests=[], margin_x=5, margin_y=20)
 for i in range(len(file_list)):
-  measure_path = f"Data/temp/{file_list[i]}"
-  measure_section = mp.decode_music_score(measure_path,model_path,voc_file_path)
-  measure_list.append(measure_section)
+  result = mp.decode_music_score(file_list[i], model_path, voc_file_path)
+  sanitized_result = [ele for ele in result if ele != 'barline']
+  measure_list.append(sanitized_result)
+print(measure_list)
 print(xl.create_musicxml(measure_list))
-os.removedir(Temp_path)
