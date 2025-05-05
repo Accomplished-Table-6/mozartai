f4cb830cca279cc49880503bdeec835f8594b957
Accomplished-Table-6 <72772964+Accomplished-Table-6@users.noreply.github.com>
2025-05-04 21:10:24 -0500

    Update final_program.py

--- final_program.py
+++ final_program.py
@@ -1,19 +1,35 @@
 import mozart_predict as mp
-import xmlencoder as xl
-import tempfile
-import image_dissector as imd
-import os
+import xmlencode2 as xl
+import imd_staf as imd
+import nested_list as nl
 
-
-image_path = "Data/Example/Capture.png"
-Temp_path = "Data/Temp"
-model_path = "Models/Models/semantic_model.meta"
+song_nam = "blahblah"
+image_path = "Data/Example/Capture_1.PNG"
+Temp_path = "Data/Temp/"
+model_path = "Models/semantic_model.meta"
 voc_file_path = "Data/vocabulary_semantic.txt"
-measure_list = []
-file_list = imd.split_sheet_music_per_measure(image_path, Temp_path, multi_measure_rests=[], margin_x=5, margin_y=20)
-for i in range(len(file_list)):
-  result = mp.decode_music_score(file_list[i], model_path, voc_file_path)
-  sanitized_result = [ele for ele in result if ele != 'barline']
-  measure_list.append(sanitized_result)
-print(measure_list)
-print(xl.create_musicxml(measure_list))
+file_list = imd.split_sheet_music_into_phrases(image_path, Temp_path, margin_x=5, margin_y=20)
+phrase_list =[]
+for phrases in file_list:
+  result = mp.decode_music_score(phrases, model_path, voc_file_path)
+  prase_list = [x for x in result]
+  phrase_list.extend(prase_list)
+
+def group_measures(notes):
+    measures = []
+    current_measure = []
+
+    for item in notes:
+        if item == "barline":
+            measures.append(current_measure)
+            current_measure = []
+        else:
+            current_measure.append(item)
+
+    if current_measure:  # Add the last measure if not empty
+        measures.append(current_measure)
+
+    return measures
+finale = group_measures(phrase_list)
+print(finale)
+xl.create_musicxml(finale)
